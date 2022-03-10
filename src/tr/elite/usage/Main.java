b2d15bc06c33d4e1de254270ffa3426bf67aaaf5
Sadece Emir <86079863+Sadece-Emir@users.noreply.github.com>
2022-03-10 23:43:21 +0300

    Update Main.java

--- src/tr/elite/usage/Main.java
+++ src/tr/elite/usage/Main.java
@@ -8,13 +8,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Command.register("test",new TestCommand()); // IMP.
+        String prefix = "/";
 
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        if (text.startsWith("/")) {
+        if (text.startsWith(prefix)) {
             //#region IMPORTANT
             try {
-                Command.run(text,"/");
+                Command.run(text,prefix);
             } catch (CommandNotFoundException e) {
                 e.printStackTrace();
             }
