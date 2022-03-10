d6354e06d4c29817569c2b815b97652a9093eb8e
Sadece Emir <86079863+Sadece-Emir@users.noreply.github.com>
2022-03-10 23:46:02 +0300

    Prefix Bug
    Fixed one more length of prefix

--- src/tr/elite/api/Command.java
+++ src/tr/elite/api/Command.java
@@ -10,7 +10,7 @@ public class Command {
     }
 
     public static void run(String fullText, String prefix) throws CommandNotFoundException {
-        String command = fullText.substring(1).split(" ")[0];
+        String command = fullText.substring(prefix.length()).split(" ")[0];
         String[] args = fullText.split(" ").length > 1 ? fullText.substring(prefix.length()).split(command + " ")[1].split(" ") : new String[0];
         if (commands.containsKey(command)) {
             commands.get(command).onCommand(command,args);
